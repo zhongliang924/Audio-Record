7ee4c37fc26eb11b685b42daf92814ed07f8b469
ZhongliangLi-github <1551054080@qq.com>
2023-07-19 22:21:44 +0800

    new function

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.myapplication
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.pm.PackageManager
 import android.media.AudioFormat
 import android.media.AudioRecord
+import android.media.MediaPlayer
 import android.media.MediaRecorder
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.core.app.ActivityCompat
 import okhttp3.OkHttpClient
@@ -17,40 +21,79 @@ import okhttp3.WebSocket
 import okhttp3.WebSocketListener
 import okio.ByteString.Companion.toByteString
 
+
 class MainActivity : ComponentActivity() {
     companion object {
+        private const val TAG = "MainActivity"
         private const val WS_URL = "ws://192.168.31.241:8080/ws"     // WebSocket 服务器地址
 
-        private const val CHANNEL_CONFIG = AudioFormat.CHANNEL_IN_MONO;     // 录音通道设置
-        private const val AUDIO_FORMAT = AudioFormat.ENCODING_PCM_16BIT;    // 录音格式
+        private const val CHANNEL_CONFIG = AudioFormat.CHANNEL_IN_MONO     // 录音通道设置
+        private const val AUDIO_FORMAT = AudioFormat.ENCODING_PCM_16BIT    // 录音格式
         private const val SAMPLE_RATE = 16000 // 采样率
     }
 
     private var audioRecord: AudioRecord? = null
     private var readThread: Thread? = null
-    private var isRecording = false
 
     private var webSocket: WebSocket? = null
 
+    private var recordingStartTime: Long = 0
+    private var recordDurationMs = 3000 // 指定录音时长 （毫秒）
+
+    private lateinit var messageTextView: TextView
+    private lateinit var startPlayer: MediaPlayer
+    private lateinit var stopPlayer: MediaPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        verifyPermissions(this)
+        init()
+    }
 
-        val btnRecord = findViewById<Button>(R.id.btn_record)
+    /**
+     * 初始化
+     */
+    private fun init() {
+        setContentView(R.layout.activity_main)
+        val btnRecord = findViewById<Button>(R.id.start)
+        val btnStop = findViewById<Button>(R.id.stop)
+
+        messageTextView = findViewById(R.id.messageTextView)
+
+        startPlayer = MediaPlayer.create(this, R.raw.ding)
+        stopPlayer = MediaPlayer.create(this, R.raw.dong)
+
+        // 开始录音事件触发
         btnRecord.setOnClickListener {
-            if (!isRecording){
-                // 开始录音
-                startRecord()
-                // 连接 WebSocket 服务器
-                connectWebSocket()
-                isRecording = true
-            } else {
-                // 停止录音
-                stopRecord()
-                // 断开 WebSocket 连接
-                disconnectWebSocket()
-                isRecording = false
-            }
+            startPlayer.start()
+            Thread.sleep(1500)      // 添加了 1.5s 延时，这个延时是等待提示音播完
+            startRecord()
+            connectWebSocket()
+        }
+        // 停止录音事件出发
+        btnStop.setOnClickListener {
+            stopRecord()
+            stopPlayer.start()
+            disconnectWebSocket()
+        }
+    }
+
+    /**
+     * 申请录音权限
+     */
+    private fun verifyPermissions(activity: Activity?) {
+        val GET_RECODE_AUDIO = 1
+        val PERMISSION_ALL = arrayOf(
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.MODIFY_AUDIO_SETTINGS
+        )
+        val permission = ActivityCompat.checkSelfPermission(activity!!, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(activity, Manifest.permission.MODIFY_AUDIO_SETTINGS) != PackageManager.PERMISSION_GRANTED
+        if (permission) {
+            ActivityCompat.requestPermissions(
+                activity, PERMISSION_ALL,
+                GET_RECODE_AUDIO
+            )
         }
     }
 
@@ -58,6 +101,8 @@ class MainActivity : ComponentActivity() {
      * 开始录音
      */
     private fun startRecord() {
+        Log.d(TAG, "StartRecording:")
+
         val minBufferSize = AudioRecord.getMinBufferSize(SAMPLE_RATE, CHANNEL_CONFIG, AUDIO_FORMAT) * 2
 
         if (ActivityCompat.checkSelfPermission(
@@ -76,11 +121,11 @@ class MainActivity : ComponentActivity() {
         }
         audioRecord = AudioRecord(MediaRecorder.AudioSource.MIC, SAMPLE_RATE, CHANNEL_CONFIG, AUDIO_FORMAT, minBufferSize)
 
-        val buffer = ByteArray(minBufferSize)
-        audioRecord?.startRecording()
-
+        recordingStartTime = System.currentTimeMillis()
         readThread = Thread {
-            while (isRecording) {
+            val buffer = ByteArray(minBufferSize)
+            audioRecord?.startRecording()
+            while (System.currentTimeMillis() - recordingStartTime < recordDurationMs) {
                 val readBytes = audioRecord?.read(buffer, 0, buffer.size) ?: 0
                 if (readBytes > 0 && webSocket != null) {
                     // 发送录音数据到 WebSocket 服务器
@@ -98,8 +143,8 @@ class MainActivity : ComponentActivity() {
     /**
      * 停止录音
      */
+    @SuppressLint("SetTextI18n")
     private fun stopRecord() {
-        isRecording = false
         // 等待读取线程完成执行，确保所有数据都已被写入并读取完毕
         try {
             readThread?.join(1000)
@@ -109,6 +154,7 @@ class MainActivity : ComponentActivity() {
         // 停止录音并释放相关资源
         audioRecord?.stop()
         audioRecord?.release()
+        disconnectWebSocket()
     }
 
     /**
@@ -119,12 +165,13 @@ class MainActivity : ComponentActivity() {
         webSocket = OkHttpClient().newWebSocket(request, object : WebSocketListener() {
 
             override fun onOpen(webSocket: WebSocket, response: Response) {
-                webSocket.send("Hello, WebSocket!")
                 Log.d("WebSocket Info", "onOpen")
             }
 
             override fun onMessage(webSocket: WebSocket, text: String) {
                 Log.d("WebSocket Info", "OnMessage: $text")
+                // 接收到服务器的消息，更新 UI 显示
+                runOnUiThread { messageTextView.setText(text) }
             }
 
             override fun onClosing(webSocket: WebSocket, code: Int, reason: String) {
